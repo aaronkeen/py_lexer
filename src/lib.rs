0bfbde127659f91c534291a2cc3b0fc708fc88d6
Aaron Keen <aaronkeen@gmail.com>
2015-10-14 13:05:49 +0200

    Restructuring is coming along.  Restored processing of identifiers and newlines.  Now have an initial implementation of explicit line joining.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,13 @@
-use std::str::{Chars, LinesAny};
-use std::iter::{Peekable, Enumerate, Map};
+use std::str::{Chars};
+use std::iter::{Peekable};
 
 const TAB_STOP_SIZE: u32 = 8;
 
 pub struct Lexer<'a>
 {
    indent_stack: Vec<u32>,
-   lines: Box<Iterator<Item=Line<'a>> + 'a>
+   lines: Box<Iterator<Item=Line<'a>> + 'a>,
+   current_line: Option<Line<'a>>,
 }
 
 struct Line<'a>
@@ -30,129 +31,108 @@ impl <'a> Lexer<'a>
    fn new<'b, I>(lines: I) -> Lexer<'b>
       where I: Iterator<Item=&'b str> + 'b
    {
-      let iter = lines
-         .enumerate()
+      let mut iter = (1..).zip(lines)
          .map(|(n, line)| (n, line.chars().peekable()))
          .map(|(n, mut chars)|
             Line::new(n, count_indentation(&mut chars), chars));
       ;
+      let first_line = iter.next();
 
       Lexer{indent_stack: vec![],
-         lines: Box::new(iter)
+         lines: Box::new(iter),
+         current_line: first_line,
       }
    }
 
-/*
-   fn next_token(&mut self) -> Option<(u32, String)>
+   fn next_token(&mut self) -> Option<(usize, String)>
    {
-      match self.chars.peek()
+      if let Some(mut current_line) = self.current_line.take()
       {
-         Some(&c) if Lexer::is_xid_start(c) =>
-            {
-               let result = self.build_identifier();
-               self.consume_space_to_next();
-               Some(result)
-            },
-         Some(&c) if Lexer::is_newline(c) => Some(self.build_newline()),
-         _ => None
+         match current_line.chars.peek()
+         {
+            Some(&c) if is_xid_start(c) =>
+               {
+                  let result = self.build_identifier(&mut current_line);
+                  self.consume_space_to_next(&mut current_line);
+                  self.current_line = Some(current_line);
+                  Some(result)
+               },
+            Some(&c) if c == '\\' =>
+               {
+                  current_line.chars.next();
+                  if current_line.chars.peek().is_none()
+                  {
+                     // explicit line join
+                     self.current_line = self.lines.next();
+                     self.next_token()
+                  }
+                  else
+                  {
+                     let line_number = current_line.number;
+                     self.consume_space_to_next(&mut current_line);
+                     self.current_line = Some(current_line);
+                     Some((line_number, "** bad \\ **".to_string()))
+                  }
+               }
+            Some(_) => None,
+            None =>
+               {
+                  self.current_line = self.lines.next();
+                  // indentation check here
+                  Some((current_line.number, "*newline*".to_string()))
+               }
+         }
+      }
+      else
+      {
+         None
       }
    }
 
-   fn build_identifier(&mut self) -> (u32, String)
+   fn build_identifier(&mut self, current_line: &mut Line) -> (usize, String)
    {
       let mut token = String::new();
-      token.push(self.chars.next().unwrap());
+      token.push(current_line.chars.next().unwrap());
 
-      self.char_number += 1;
-
-      while self.chars.peek().is_some() &&
-         Lexer::is_xid_continue(*self.chars.peek().unwrap())
+      while current_line.chars.peek().is_some() &&
+         is_xid_continue(*current_line.chars.peek().unwrap())
       {
-         token.push(self.chars.next().unwrap());
-         self.char_number += 1;
+         token.push(current_line.chars.next().unwrap());
       }
 
-      (self.line_number, token)
+      (current_line.number, token)
    }
 
-   fn build_newline(&mut self) -> (u32, String)
+   fn consume_space_to_next(&mut self, current_line: &mut Line)
    {
-      let c = self.chars.next().unwrap();
-
-      if c == '\r' && self.chars.peek().is_some() &&
-         *self.chars.peek().unwrap() == '\n'
+      while current_line.chars.peek().is_some() &&
+         is_space(*current_line.chars.peek().unwrap())
       {
-         self.chars.next();
-      }
-
-      let line_number = self.line_number;
-      self.line_number += 1;
-      self.char_number = 0;
-
-      (line_number, "newline".to_string())
-   }
-
-   fn consume_space_to_next(&mut self)
-   {
-      while self.chars.peek().is_some() &&
-         Lexer::is_space_between(*self.chars.peek().unwrap())
-      {
-         self.chars.next();
-         self.char_number += 1;
+         current_line.chars.next();
       }
    }
-
-
-   /// This function should be modified to do a more appropriate unicode
-   /// check.  Eliding for now due to apparently unstable support in Rust.
-   fn is_xid_start(c: char) -> bool
-   {
-      c.is_alphabetic() || c == '_'
-   }
-
-   /// This function should be modified to do a more appropriate unicode
-   /// check.  Eliding for now due to apparently unstable support in Rust.
-   fn is_xid_continue(c: char) -> bool
-   {
-      c.is_alphanumeric() || c == '_'
-   }
-
-   fn is_space_between(c: char) -> bool
-   {
-      c == ' ' || c == '\t' || c == '\x0C'
-   }
-
-   fn is_newline(c: char) -> bool
-   {
-      c == '\r' || c == '\n'
-   }
-*/
 }
 
-/*
 impl <'a> Iterator for Lexer<'a>
 {
-   type Item = (u32, String);
+   type Item = (usize, String);
 
    fn next(&mut self) -> Option<Self::Item>
    {
-      match self.chars.peek()
-      {
-         None => None,
-         Some(_) => self.next_token()
-      }
+      self.next_token()
    }
 }
-*/
 
 fn determine_spaces(char_count: u32, tab_stop_size: u32) -> u32
 {
    tab_stop_size - char_count % tab_stop_size
 }
 
+/// This function currently considers \r as a whitespace character instead
+/// of an old Mac end of line character.
 fn is_space(c: char) -> bool
 {
-   c == ' ' || c == '\t' || c == '\x0C'
+   c == ' ' || c == '\t' || c == '\x0C' || c == '\r' // ignore \r for now
 }
 
 fn process_character(count: u32, c: char) -> u32
@@ -187,8 +167,20 @@ fn count_indentation(chars: &mut Peekable<Chars>) -> u32
    count
 }
 
+/// This function should be modified to do a more appropriate unicode
+/// check.  Eliding for now due to apparently unstable support in Rust.
+fn is_xid_start(c: char) -> bool
+{
+   c.is_alphabetic() || c == '_'
+}
 
-/*
+/// This function should be modified to do a more appropriate unicode
+/// check.  Eliding for now due to apparently unstable support in Rust.
+fn is_xid_continue(c: char) -> bool
+{
+   c.is_alphanumeric() || c == '_'
+}
+
 
 #[cfg(test)]
 mod tests
@@ -196,27 +188,23 @@ mod tests
    use super::Lexer;
 
    #[test]
-   fn test_creation()
-   {
-      let chars: &mut Iterator<Item=char> = &mut "abcdef 123".chars();
-      let l = Lexer::new(chars);
-      assert_eq!(l.chars.collect::<Vec<char>>(),
-         vec!['a', 'b', 'c', 'd', 'e', 'f', ' ', '1', '2', '3']);
-   }   
-
-   #[test]
    fn test_identifiers()
    {
-      let chars = &mut "abf  \x0C xyz 	e2f  \rmq3\n12\r\n3\r23\n".chars() as
-         &mut Iterator<Item=char>;
-      let mut l = Lexer::new(chars);
+      let chars = &mut "abf  \x0C xyz 	e2f  mq3\nn12\\\r\nn3\\ n23\n";
+      let mut l = Lexer::new(chars.lines_any());
       assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((1, "abf")));
       assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((1, "xyz")));
       assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((1, "e2f")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((1, "newline")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((2, "mq3")));
+      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((1, "mq3")));
+      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((1, "*newline*")));
+      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((2, "n12")));
+      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((3, "n3")));
+      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((3, "** bad \\ **")));
+      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((3, "n23")));
+      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((3, "*newline*")));
    }   
 
+   /*
    #[test]
    fn test_line_numbers()
    {
@@ -235,5 +223,5 @@ mod tests
       assert_eq!(l.next(), Some(4));
       assert_eq!(l.next(), Some(4));
    }   
-}
 */
+}
