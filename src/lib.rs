5e5164a6e18dd70976c56f85e0eb8de73ca783dd
Aaron Keen <aaronkeen@gmail.com>
2015-10-15 11:43:25 +0200

    Added a ResultToken type to allow for error reporting.
    Partial implementation of number tokens.

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,8 @@ use std::iter::{Peekable};
 
 const TAB_STOP_SIZE: u32 = 8;
 
+pub type ResultToken = Result<String, String>;
+
 pub struct Lexer<'a>
 {
    indent_stack: Vec<u32>,
@@ -47,7 +49,7 @@ impl <'a> Lexer<'a>
       }
    }
 
-   fn next_token(&mut self) -> Option<(usize, String)>
+   fn next_token(&mut self) -> Option<(usize, ResultToken)>
    {
       let current_line = self.current_line.take();
       let result = self.next_token_line(current_line);
@@ -56,7 +58,7 @@ impl <'a> Lexer<'a>
    }
 
    fn next_token_line(&mut self, current_line: Option<Line<'a>>)
-      -> (Option<(usize, String)>, Option<Line<'a>>)
+      -> (Option<(usize, ResultToken)>, Option<Line<'a>>)
    {
       if let Some(mut current_line) = current_line
       {
@@ -71,6 +73,8 @@ impl <'a> Lexer<'a>
             {
                Some(&c) if is_xid_start(c) =>
                   self.process_identifier(current_line),
+               Some(&c) if c.is_digit(10) || c == '.' =>
+                  self.process_number(current_line),
                Some(&c) if c == '\\' =>
                   self.process_line_join(current_line),
                Some(_) => (None, Some(current_line)),
@@ -84,20 +88,21 @@ impl <'a> Lexer<'a>
          match self.lines.next()
          {
             None if self.indent_stack.len() <= 1 => (None, None),
-            None => (Some((0, "** DEDENT **".to_string())), None),
+            None => (Some((0, Ok("** DEDENT **".to_string()))), None),
             Some(newline) => self.process_line_start(newline)
          }
       }
    }
 
    fn process_identifier(&self, mut current_line: Line<'a>)
-      -> (Option<(usize, String)>, Option<Line<'a>>)
+      -> (Option<(usize, ResultToken)>, Option<Line<'a>>)
    {
       let result = self.build_identifier(&mut current_line);
       (Some(result), Some(current_line))
    }
 
-   fn build_identifier(&self, current_line: &mut Line) -> (usize, String)
+   fn build_identifier(&self, current_line: &mut Line)
+      -> (usize, ResultToken)
    {
       let mut token = String::new();
       token.push(current_line.chars.next().unwrap());
@@ -108,7 +113,97 @@ impl <'a> Lexer<'a>
          token.push(current_line.chars.next().unwrap());
       }
 
-      (current_line.number, token)
+      (current_line.number, Ok(token))
+   }
+
+   fn process_number(&self, mut current_line: Line<'a>)
+      -> (Option<(usize, ResultToken)>, Option<Line<'a>>)
+   {
+      let result = self.build_number(&mut current_line);
+      (Some(result), Some(current_line))
+   }
+
+   fn build_number(&self, line: &mut Line<'a>) -> (usize, ResultToken)
+   {
+      let first = *line.chars.peek().unwrap();
+
+      if first == '0'
+      {
+         let result = self.build_zero_prefixed_number(line);
+         (line.number, result)
+      }
+      else if first == '.'
+      {
+         let result =
+            self.build_point_float(Ok(String::new()), line);
+         //let result = self.build_exp_float(result, line);
+         (line.number, result)
+      }
+      else
+      {
+         let result = self.build_decimal_number(line);
+         let result = self.build_point_float(result, line);
+         //let result = self.build_exp_float(result, line);
+
+         (line.number, result)
+      }
+   }
+
+   fn build_decimal_number(&self, line: &mut Line<'a>)
+      -> ResultToken
+   {
+      let mut token = String::new();
+
+      token.push(line.chars.next().unwrap());
+
+      while line.chars.peek().is_some() &&
+         line.chars.peek().unwrap().is_digit(10)
+      {
+         token.push(line.chars.next().unwrap());
+      }
+
+      Ok(token)
+   }
+
+   fn build_zero_prefixed_number(&self, line: &mut Line<'a>)
+      -> ResultToken
+   {
+
+   }
+
+   fn build_point_float(&self, rtoken: ResultToken, line: &mut Line<'a>)
+      -> ResultToken
+   {
+      if rtoken.is_err()
+      {
+         return rtoken;
+      }
+
+      if line.chars.peek().is_none() ||
+         *line.chars.peek().unwrap() != '.'
+      {
+         return rtoken;
+      }
+
+      let mut token = rtoken.ok().unwrap();
+
+      token.push(line.chars.next().unwrap());
+
+      if line.chars.peek().is_some() &&
+         line.chars.peek().unwrap().is_digit(10)
+      {
+         while line.chars.peek().is_some() &&
+            line.chars.peek().unwrap().is_digit(10)
+         {
+            token.push(line.chars.next().unwrap());
+         }
+
+         Ok(token)
+      }
+      else
+      {
+         Err("** invalid number: ".to_string() + &token)
+      }
    }
 
    fn consume_space_to_next(&self, current_line: &mut Line)
@@ -121,7 +216,7 @@ impl <'a> Lexer<'a>
    }
 
    fn process_line_join(&mut self, mut current_line: Line<'a>)
-      -> (Option<(usize, String)>, Option<Line<'a>>)
+      -> (Option<(usize, ResultToken)>, Option<Line<'a>>)
    {
       current_line.chars.next();
       if current_line.chars.peek().is_none()
@@ -133,18 +228,19 @@ impl <'a> Lexer<'a>
       else
       {
          let line_number = current_line.number;
-         (Some((line_number, "** bad \\ **".to_string())), Some(current_line))
+         (Some((line_number, Err("** bad \\ **".to_string()))),
+            Some(current_line))
       }
    }
 
    fn process_newline(&self, current_line: Line<'a>)
-      -> (Option<(usize, String)>, Option<Line<'a>>)
+      -> (Option<(usize, ResultToken)>, Option<Line<'a>>)
    {
-      (Some((current_line.number, "*newline*".to_string())), None)
+      (Some((current_line.number, Ok("*newline*".to_string()))), None)
    }
 
    fn process_line_start(&mut self, mut newline: Line<'a>)
-      -> (Option<(usize, String)>, Option<Line<'a>>)
+      -> (Option<(usize, ResultToken)>, Option<Line<'a>>)
    {
       if let Some(&previous_indent) = self.indent_stack.last()
       {
@@ -155,7 +251,8 @@ impl <'a> Lexer<'a>
          else if newline.indentation > previous_indent
          {
             self.indent_stack.push(newline.indentation);
-            (Some((newline.number, "** INDENT **".to_string())), Some(newline))
+            (Some((newline.number, Ok("** INDENT **".to_string()))),
+               Some(newline))
          }
          else if newline.indentation < previous_indent
          {
@@ -185,18 +282,18 @@ impl <'a> Lexer<'a>
    }
 
    fn process_dedents(&mut self, current_line: Line<'a>)
-      -> (Option<(usize, String)>, Option<Line<'a>>)
+      -> (Option<(usize, ResultToken)>, Option<Line<'a>>)
    {
       if self.dedent_count == -1
       {
          self.dedent_count = 0;
-         (Some((current_line.number, "** DEDENT ERROR **".to_string())),
+         (Some((current_line.number, Ok("** DEDENT ERROR **".to_string()))),
             Some(current_line))
       }
       else
       {
          self.dedent_count += if self.dedent_count < 0 {1} else {-1};
-         (Some((current_line.number, "** DEDENT **".to_string())),
+         (Some((current_line.number, Ok("** DEDENT **".to_string()))),
             Some(current_line))
       }
    }
@@ -204,7 +301,7 @@ impl <'a> Lexer<'a>
 
 impl <'a> Iterator for Lexer<'a>
 {
-   type Item = (usize, String);
+   type Item = (usize, ResultToken);
 
    fn next(&mut self) -> Option<Self::Item>
    {
@@ -281,32 +378,57 @@ mod tests
    {
       let chars = &mut "abf  \x0C _xyz\n   \n  e2f\n  \tmq3\nn12\\\r\nn3\\ \n  n23\n    n24\n   n25\n";
       let mut l = Lexer::new(chars.lines_any());
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((1, "abf")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((1, "_xyz")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((1, "*newline*")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((3, "** INDENT **")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((3, "e2f")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((3, "*newline*")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((4, "** INDENT **")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((4, "mq3")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((4, "*newline*")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((5, "** DEDENT **")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((5, "** DEDENT **")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((5, "n12")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((6, "n3")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((6, "** bad \\ **")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((6, "*newline*")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((7, "** INDENT **")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((7, "n23")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((7, "*newline*")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((8, "** INDENT **")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((8, "n24")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((8, "*newline*")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((9, "** DEDENT ERROR **")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((9, "n25")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((9, "*newline*")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((0, "** DEDENT **")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((0, "** DEDENT **")));
+      assert_eq!(l.next(), Some((1, Ok("abf".to_string()))));
+      assert_eq!(l.next(), Some((1, Ok("_xyz".to_string()))));
+      assert_eq!(l.next(), Some((1, Ok("*newline*".to_string()))));
+      assert_eq!(l.next(), Some((3, Ok("** INDENT **".to_string()))));
+      assert_eq!(l.next(), Some((3, Ok("e2f".to_string()))));
+      assert_eq!(l.next(), Some((3, Ok("*newline*".to_string()))));
+      assert_eq!(l.next(), Some((4, Ok("** INDENT **".to_string()))));
+      assert_eq!(l.next(), Some((4, Ok("mq3".to_string()))));
+      assert_eq!(l.next(), Some((4, Ok("*newline*".to_string()))));
+      assert_eq!(l.next(), Some((5, Ok("** DEDENT **".to_string()))));
+      assert_eq!(l.next(), Some((5, Ok("** DEDENT **".to_string()))));
+      assert_eq!(l.next(), Some((5, Ok("n12".to_string()))));
+      assert_eq!(l.next(), Some((6, Ok("n3".to_string()))));
+      assert_eq!(l.next(), Some((6, Err("** bad \\ **".to_string()))));
+      assert_eq!(l.next(), Some((6, Ok("*newline*".to_string()))));
+      assert_eq!(l.next(), Some((7, Ok("** INDENT **".to_string()))));
+      assert_eq!(l.next(), Some((7, Ok("n23".to_string()))));
+      assert_eq!(l.next(), Some((7, Ok("*newline*".to_string()))));
+      assert_eq!(l.next(), Some((8, Ok("** INDENT **".to_string()))));
+      assert_eq!(l.next(), Some((8, Ok("n24".to_string()))));
+      assert_eq!(l.next(), Some((8, Ok("*newline*".to_string()))));
+      assert_eq!(l.next(), Some((9, Ok("** DEDENT ERROR **".to_string()))));
+      assert_eq!(l.next(), Some((9, Ok("n25".to_string()))));
+      assert_eq!(l.next(), Some((9, Ok("*newline*".to_string()))));
+      assert_eq!(l.next(), Some((0, Ok("** DEDENT **".to_string()))));
+      assert_eq!(l.next(), Some((0, Ok("** DEDENT **".to_string()))));
+   }   
+
+   #[test]
+   fn test_numbers()
+   {
+      let chars = &mut "123 456 45 23.742 23. 12..3 .14 0123.2192 12e17 12e+17 12E-17 00000 00003 0o724 0X32facb7 0b10101010\n";
+      let mut l = Lexer::new(chars.lines_any());
+      assert_eq!(l.next(), Some((1, Ok("123".to_string()))));
+      assert_eq!(l.next(), Some((1, Ok("456".to_string()))));
+      assert_eq!(l.next(), Some((1, Ok("45".to_string()))));
+      assert_eq!(l.next(), Some((1, Ok("23.742".to_string()))));
+      assert_eq!(l.next(), Some((1, Err("** invalid number: 23.".to_string()))));
+      assert_eq!(l.next(), Some((1, Err("** invalid number: 12.".to_string()))));
+      assert_eq!(l.next(), Some((1, Ok(".3".to_string()))));
+      assert_eq!(l.next(), Some((1, Ok(".14".to_string()))));
+      assert_eq!(l.next(), Some((1, Ok("0123.2192".to_string()))));
+      assert_eq!(l.next(), Some((1, Ok("12e17".to_string()))));
+      assert_eq!(l.next(), Some((1, Ok("12e+17".to_string()))));
+      assert_eq!(l.next(), Some((1, Ok("12E-17".to_string()))));
+      assert_eq!(l.next(), Some((1, Ok("00000".to_string()))));
+      assert_eq!(l.next(), Some((1, Err("00003".to_string()))));
+      assert_eq!(l.next(), Some((1, Ok("0o724".to_string()))));
+      assert_eq!(l.next(), Some((1, Ok("0X32facb7".to_string()))));
+      assert_eq!(l.next(), Some((1, Ok("0b10101010".to_string()))));
+      assert_eq!(l.next(), Some((1, Ok("*newline*".to_string()))));
    }   
 
    #[test]
@@ -314,41 +436,20 @@ mod tests
    {
       let chars = &mut "    abf xyz\n\n\n\n        e2f\n             n12\n  n2\n";
       let mut l = Lexer::new(chars.lines_any());
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((1, "** INDENT **")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((1, "abf")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((1, "xyz")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((1, "*newline*")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((5, "** INDENT **")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((5, "e2f")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((5, "*newline*")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((6, "** INDENT **")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((6, "n12")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((6, "*newline*")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((7, "** DEDENT **")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((7, "** DEDENT **")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((7, "** DEDENT ERROR **")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((7, "n2")));
-      assert_eq!(l.next().as_ref().map(|p| (p.0, &p.1[..])), Some((7, "*newline*")));
+      assert_eq!(l.next(), Some((1, Ok("** INDENT **".to_string()))));
+      assert_eq!(l.next(), Some((1, Ok("abf".to_string()))));
+      assert_eq!(l.next(), Some((1, Ok("xyz".to_string()))));
+      assert_eq!(l.next(), Some((1, Ok("*newline*".to_string()))));
+      assert_eq!(l.next(), Some((5, Ok("** INDENT **".to_string()))));
+      assert_eq!(l.next(), Some((5, Ok("e2f".to_string()))));
+      assert_eq!(l.next(), Some((5, Ok("*newline*".to_string()))));
+      assert_eq!(l.next(), Some((6, Ok("** INDENT **".to_string()))));
+      assert_eq!(l.next(), Some((6, Ok("n12".to_string()))));
+      assert_eq!(l.next(), Some((6, Ok("*newline*".to_string()))));
+      assert_eq!(l.next(), Some((7, Ok("** DEDENT **".to_string()))));
+      assert_eq!(l.next(), Some((7, Ok("** DEDENT **".to_string()))));
+      assert_eq!(l.next(), Some((7, Ok("** DEDENT ERROR **".to_string()))));
+      assert_eq!(l.next(), Some((7, Ok("n2".to_string()))));
+      assert_eq!(l.next(), Some((7, Ok("*newline*".to_string()))));
    }   
-
-   /*
-   #[test]
-   fn test_line_numbers()
-   {
-      let chars = &mut "abf\n12\r\n3\r23\n".chars() as
-         &mut Iterator<Item=char>;
-      let mut l = Lexer::new(chars).map(|(n,_)| n);
-      assert_eq!(l.next(), Some(1));
-      assert_eq!(l.next(), Some(1));
-      assert_eq!(l.next(), Some(1));
-      assert_eq!(l.next(), Some(2));
-      assert_eq!(l.next(), Some(2));
-      assert_eq!(l.next(), Some(2));
-      assert_eq!(l.next(), Some(3));
-      assert_eq!(l.next(), Some(3));
-      assert_eq!(l.next(), Some(4));
-      assert_eq!(l.next(), Some(4));
-      assert_eq!(l.next(), Some(4));
-   }   
-*/
 }
