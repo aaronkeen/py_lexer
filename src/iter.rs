75e00cae0390c09520246ccc2548f6e27463d996
Aaron Keen <aaronkeen@gmail.com>
2015-10-21 15:29:48 +0200

    Implementation of an iterator that allows peeking at first two elements. This is necessary to handle the '...' token since '..' is not allowed; as such, '..' should technically be two Dot tokens, even though that will not be allowed by the parser.

---
+++ src/iter.rs
@@ -0,0 +1,126 @@
+use std::iter::Iterator;
+
+pub struct DoublePeekable<I>
+   where I: Iterator
+{
+   first: Option<I::Item>,
+   second: Option<I::Item>,
+   iter: I,
+}
+
+impl <I> DoublePeekable<I>
+   where I: Iterator
+{
+   fn new(mut iter: I)
+      -> Self
+   {
+      let first = iter.next();
+      let second = iter.next();
+      DoublePeekable{iter: iter, first: first, second: second}
+   }
+
+   fn peek_first(&self)
+      -> Option<&I::Item>
+   {
+      self.first.as_ref()
+   }
+
+   fn peek_second(&self)
+      -> Option<&I::Item>
+   {
+      self.second.as_ref()
+   }
+
+   fn get_next(&mut self)
+      -> Option<I::Item>
+   {
+      let result = self.first.take();
+
+      self.first = self.second.take();
+      if self.first.is_some()
+      {
+         // technically, an interator is not required to return None
+         // if next is called again after None has been returned
+         self.second = self.iter.next();
+      }
+      result
+   }
+}
+
+impl <I> Iterator for DoublePeekable<I>
+   where I: Iterator
+{
+   type Item = I::Item;
+
+   fn next(&mut self)
+      -> Option<I::Item>
+   {
+      self.get_next()
+   }
+}
+
+#[cfg(test)]
+mod test
+{
+   use super::DoublePeekable;
+   #[test]
+   fn test_peek()
+   {
+      let mut iter = DoublePeekable::new(1..6);
+      assert_eq!(2, *iter.peek_second().unwrap());
+      assert_eq!(1, *iter.peek_first().unwrap());
+      assert_eq!(1, *iter.peek_first().unwrap());
+      assert_eq!(2, *iter.peek_second().unwrap());
+      assert_eq!(2, *iter.peek_second().unwrap());
+      assert_eq!(1, *iter.peek_first().unwrap());
+      assert_eq!(1, iter.next().unwrap());
+
+      assert_eq!(3, *iter.peek_second().unwrap());
+      assert_eq!(2, *iter.peek_first().unwrap());
+      assert_eq!(2, *iter.peek_first().unwrap());
+      assert_eq!(3, *iter.peek_second().unwrap());
+      assert_eq!(3, *iter.peek_second().unwrap());
+      assert_eq!(2, *iter.peek_first().unwrap());
+      assert_eq!(2, iter.next().unwrap());
+
+      assert_eq!(4, *iter.peek_second().unwrap());
+      assert_eq!(3, *iter.peek_first().unwrap());
+      assert_eq!(3, *iter.peek_first().unwrap());
+      assert_eq!(4, *iter.peek_second().unwrap());
+      assert_eq!(4, *iter.peek_second().unwrap());
+      assert_eq!(3, *iter.peek_first().unwrap());
+      assert_eq!(3, iter.next().unwrap());
+
+      assert_eq!(5, *iter.peek_second().unwrap());
+      assert_eq!(4, *iter.peek_first().unwrap());
+      assert_eq!(4, *iter.peek_first().unwrap());
+      assert_eq!(5, *iter.peek_second().unwrap());
+      assert_eq!(5, *iter.peek_second().unwrap());
+      assert_eq!(4, *iter.peek_first().unwrap());
+      assert_eq!(4, iter.next().unwrap());
+
+      assert_eq!(None, iter.peek_second());
+      assert_eq!(5, *iter.peek_first().unwrap());
+      assert_eq!(5, *iter.peek_first().unwrap());
+      assert_eq!(None, iter.peek_second());
+      assert_eq!(None, iter.peek_second());
+      assert_eq!(5, *iter.peek_first().unwrap());
+      assert_eq!(5, iter.next().unwrap());
+
+      assert_eq!(None, iter.peek_second());
+      assert_eq!(None, iter.peek_first());
+      assert_eq!(None, iter.peek_first());
+      assert_eq!(None, iter.peek_second());
+      assert_eq!(None, iter.peek_second());
+      assert_eq!(None, iter.peek_first());
+      assert_eq!(None, iter.next());
+
+      assert_eq!(None, iter.peek_second());
+      assert_eq!(None, iter.peek_first());
+      assert_eq!(None, iter.peek_first());
+      assert_eq!(None, iter.peek_second());
+      assert_eq!(None, iter.peek_second());
+      assert_eq!(None, iter.peek_first());
+      assert_eq!(None, iter.next());
+   }
+}
