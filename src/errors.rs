693f54c841d0efbfc16a1563e6673f1ff72caf29
Aaron Keen <aaronkeen@gmail.com>
2015-11-09 12:31:40 +0100

    Added default error messages for lexing errors.

--- src/errors.rs
+++ src/errors.rs
@@ -17,3 +17,32 @@ pub enum Error
    Internal(String),
 }
 
+impl Error
+{
+   pub fn message(self)
+      -> String
+   {
+      match self
+      {
+         Error::BadLineContinuation => "bad line continuation".to_string(),
+         Error::UnterminatedTripleString =>
+            "unterminated triple-quoted string".to_string(),
+         Error::UnterminatedString => "unterminated string".to_string(),
+         Error::InvalidCharacter(c) => format!("invalid character {}", c),
+         Error::Dedent => "misaligned dedent".to_string(),
+         Error::HexEscapeShort => "missing digits in hex escape".to_string(),
+         Error::MalformedUnicodeEscape =>
+            "malformed unicode escape".to_string(),
+         Error::MalformedNamedUnicodeEscape =>
+            "malformed named unicode escape".to_string(),
+         Error::UnknownUnicodeName(s) =>
+            format!("unknown unicode name '{}'", s),
+         Error::MissingDigits => "missing digits".to_string(),
+         Error::MalformedFloat =>
+            "malformed floating point number".to_string(),
+         Error::MalformedImaginary => "malformed imaginary number".to_string(),
+         Error::InvalidSymbol(c) => format!("invalid symbol '{}'", c),
+         Error::Internal(s) => format!("internal error: {}", s),
+      }
+   }
+}
