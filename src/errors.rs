a061554a9187efa402a4ac12a085491146a2c396
Aaron Keen <aaronkeen@gmail.com>
2015-11-12 11:31:04 +0100

    Changed LexerError implementations to implement Error trait.

--- src/errors.rs
+++ src/errors.rs
@@ -1,3 +1,6 @@
+use std::error;
+use std::fmt;
+
 #[derive(Debug, PartialEq, Clone)]
 pub enum LexerError
 {
@@ -17,35 +20,68 @@ pub enum LexerError
    Internal(String),
 }
 
-impl LexerError
+impl fmt::Display for LexerError
 {
-   pub fn message(self)
-      -> String
+   fn fmt(&self, f: &mut fmt::Formatter)
+      -> fmt::Result
    {
-      match self
+      match *self
       {
          LexerError::BadLineContinuation =>
-            "bad line continuation".to_owned(),
+            write!(f, "bad line continuation"),
          LexerError::UnterminatedTripleString =>
-            "unterminated triple-quoted string".to_owned(),
-         LexerError::UnterminatedString => "unterminated string".to_owned(),
-         LexerError::InvalidCharacter(c) => format!("invalid character {}", c),
-         LexerError::Dedent => "misaligned dedent".to_owned(),
+            write!(f, "unterminated triple-quoted string"),
+         LexerError::UnterminatedString =>
+            write!(f, "unterminated string"),
+         LexerError::InvalidCharacter(ref c) =>
+            write!(f, "invalid character '{}'", c),
+         LexerError::Dedent =>
+            write!(f, "misaligned dedent"),
          LexerError::HexEscapeShort =>
-            "missing digits in hex escape".to_owned(),
+            write!(f, "missing digits in hex escape"),
          LexerError::MalformedUnicodeEscape =>
-            "malformed unicode escape".to_owned(),
+            write!(f, "malformed unicode escape"),
          LexerError::MalformedNamedUnicodeEscape =>
-            "malformed named unicode escape".to_owned(),
-         LexerError::UnknownUnicodeName(s) =>
-            format!("unknown unicode name '{}'", s),
-         LexerError::MissingDigits => "missing digits".to_owned(),
+            write!(f, "malformed named unicode escape"),
+         LexerError::UnknownUnicodeName(ref s) =>
+            write!(f, "unknown unicode name '{}'", s),
+         LexerError::MissingDigits =>
+            write!(f, "missing digits"),
          LexerError::MalformedFloat =>
-            "malformed floating point number".to_owned(),
+            write!(f, "malformed floating point number"),
          LexerError::MalformedImaginary =>
-            "malformed imaginary number".to_owned(),
-         LexerError::InvalidSymbol(c) => format!("invalid symbol '{}'", c),
-         LexerError::Internal(s) => format!("internal error: {}", s),
+            write!(f, "malformed imaginary number"),
+         LexerError::InvalidSymbol(ref c) =>
+            write!(f, "invalid symbol '{}'", c),
+         LexerError::Internal(ref s) =>
+            write!(f, "internal error: {}", s),
+      }
+   }
+}
+
+impl error::Error for LexerError
+{
+   fn description(&self)
+      -> &str
+   {
+      match *self
+      {
+         LexerError::BadLineContinuation => "bad line continuation",
+         LexerError::UnterminatedTripleString =>
+            "unterminated triple-quoted string",
+         LexerError::UnterminatedString => "unterminated string",
+         LexerError::InvalidCharacter(_) => "invalid character",
+         LexerError::Dedent => "misaligned dedent",
+         LexerError::HexEscapeShort => "missing digits in hex escape",
+         LexerError::MalformedUnicodeEscape => "malformed unicode escape",
+         LexerError::MalformedNamedUnicodeEscape =>
+            "malformed named unicode escape",
+         LexerError::UnknownUnicodeName(_) => "unknown unicode name",
+         LexerError::MissingDigits => "missing digits",
+         LexerError::MalformedFloat => "malformed floating point number",
+         LexerError::MalformedImaginary => "malformed imaginary number",
+         LexerError::InvalidSymbol(_) => "invalid symbol",
+         LexerError::Internal(_) => "internal error",
       }
    }
 }
